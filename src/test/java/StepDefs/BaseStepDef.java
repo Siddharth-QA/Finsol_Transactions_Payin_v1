d478eb9dd73cc7e94678563e597eeb6877176425
Siddharth <teamqa@csgtech.in>
2024-07-04 11:06:15 +0530

    Initial commit

---
+++ src/test/java/StepDefs/BaseStepDef.java
@@ -0,0 +1,9 @@
+package StepDefs;
+
+import Pages.APIv1.PayinNs2s;
+import Pages.APIv1.PayinTransaction;
+
+public class BaseStepDef {
+    protected PayinTransaction payinTransaction = new PayinTransaction();
+    protected PayinNs2s payinNs2s = new PayinNs2s();
+}
